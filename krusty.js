161f0193f04a42ece0e1d374d0441b2eb4c8fed9
ivankrat <42414346+ivankrat@users.noreply.github.com>
2019-02-03 10:24:12 -0800

    Update krusty.js

--- krusty.js
+++ krusty.js
@@ -1,13 +1,13 @@
-const discord = require("discord.js");
+const discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on("ready", () => {
-  console.log("I am ready!");
+client.on('ready', () => {
+  console.log('I am ready!');
 });
 
-client.on("message", message => {
-  if(message.content == "ping") {
-    message.reply("pong");
+client.on('message', message => {
+  if(message.content === 'KY online') {
+    message.reply('*Krusty wobbles at you*');
   }
 });
 
