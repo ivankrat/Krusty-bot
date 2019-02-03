810e7fb755dba2312937aa0114dd7f1cf2fe375d
ivankrat <42414346+ivankrat@users.noreply.github.com>
2019-02-03 10:43:17 -0800

    Update krusty.js

--- krusty.js
+++ krusty.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-  if(message.content === 'KY online') {
+  if(message.content === 'KYonline') {
     message.reply('*Krusty wobbles at you*');
   }
 });
