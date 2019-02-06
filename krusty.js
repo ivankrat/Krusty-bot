b327689621808ec659be46f2f1807f6b44445a4f
ivankrat <42414346+ivankrat@users.noreply.github.com>
2019-02-05 17:56:02 -0800

    Update krusty.js

--- krusty.js
+++ krusty.js
@@ -12,23 +12,3 @@ client.on('message', message => {
 });
 
 client.login(process.env.BOT_TOKEN);
-
-import net.dv8tion.jda.core.AccountType;
-import net.dv8tion.jda.core.JDA;
-import net.dv8tion.jda.core.JDABuilder;
-
-public class MainKrust {
-	public static void main(String[] args) throws Exception{
-		try {
-			JDA api = new JDABuilder(AccountType.BOT).setToken("NTI1ODA4OTIzMDE4Nzg4ODY0.Dv89jg.9nvsgxJkn7uqey3M8vZ9U1JZPn8").buildAsync();
-			api.addEventListener(new KYFun());
-			api.addEventListener(new KYMod());
-			api.addEventListener(new KYStats());
-			api.addEventListener(new KYPlayerTracker());
-//			api.addEventListener(new KYFilter());
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-}
